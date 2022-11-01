9b9f1077c73e05c6498693fbfe9618557cbe7b5c
Thamyresarm <pink.thamyres@gmail.com>
2022-11-01 15:54:51 -0300

    curriculo finalizado!

--- DesafiosdeProjetos/Portifolio/assets/js/api.js
+++ DesafiosdeProjetos/Portifolio/assets/js/api.js
@@ -1,4 +1,5 @@
-function fetchProfileData(){
-    const url = '';
-    fetch(url);
+async function fetchProfileData(){
+    const url = 'https://raw.githubusercontent.com/Thamyresarm/dio-formacao-JavaScript/main/DesafiosdeProjetos/Portifolio/data/profile.json';
+    const fetching = await fetch(url);
+    return fetching.json();
 }
